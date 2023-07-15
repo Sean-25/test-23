f7a6f73b07b615c3523008441043aef3b7d20460
Sean Harrison <seanharrison2510@outlook.com>
2023-07-15 23:30:37 +0100

    Store results in cache for 24 hours

--- app/Models/Characters.php
+++ app/Models/Characters.php
@@ -8,11 +8,15 @@ class Characters
     {
         $url =  "https://rickandmortyapi.com/api/character?" . http_build_query(request()->input());
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_URL, $url);
-        $results = curl_exec($ch);
-        curl_close($ch);
+        $results = cache()->remember($url, 86400, function() use ($url)
+        {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_URL, $url);
+            $results = curl_exec($ch);
+            curl_close($ch);
+            return $results;
+        });
 
         return $results ? json_decode($results, true) : NULL;
     }
