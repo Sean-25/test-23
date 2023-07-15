29b786d8ae31003c2d3ebe66ad599b897348a4a4
Sean Harrison <seanharrison2510@outlook.com>
2023-07-15 22:39:04 +0100

    Add Bootstrap and update styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,28 +5,63 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Rick and Morty Encyclopedia</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     </head>
     <body>
 
-        <h1>Rick and Morty Encyclopedia</h1>
+        <header class="my-4">
+            <div class="container-lg">
+                <a class="text-decoration-none text-reset" href="/">
+                    <h1 class="text-center text-uppercase display-1 lh-1">
+                        Rick and Morty
+                            <br>
+                        Encyclopedia
+                    </h1>
+                </a>
+            </div>
+        </header>
 
-        @isset ($characters['results'])
-            @foreach ($characters['results'] as $character)
-                <h2>{{ $character['name'] }}</h2>
-                <img src="{{ $character['image'] }}" alt="{{ $character['name'] }}" width="300" height="300">
-                <p>Species: {{ $character['species'] }}</p>
-                <p>Origin: {{ $character['origin']['name'] }}</p>
-                <p>Total Episodes: {{ count($character['episode']) }}</p>
-                <p>
-                    Episodes:
-                    @foreach ($character['episode'] as $episode)
-                        <span>{{ basename($episode) }}</span>
-                    @endforeach
-                </p>
-            @endforeach
-        @else
-            <p>No results</p>
-        @endisset
+        <main class="my-4">
+            <div class="container-lg">
+                <div class="row g-4">
+                    @isset ($characters['results'])
+                        @foreach ($characters['results'] as $character)
+                            <div class="col-lg-6">
+                                <div class="border h-100">
+                                    <div class="row h-100">
+                                        <div class="col-4">
+                                            <img src="{{ $character['image'] }}" alt="{{ $character['name'] }}" width="300" height="300" style="max-width: 100%; height: auto; display: block;">
+                                        </div>
+                                        <div class="col-8">
+                                            <div class="p-2">
+                                                <h2 class="display-6 lh-1">{{ $character['name'] }}</h2>
+                                                <p class="m-0">Species: {{ $character['species'] }}</p>
+                                                <p class="m-0">Origin: {{ $character['origin']['name'] }}</p>
+                                                <p class="m-0">Total Episodes: {{ count($character['episode']) }}</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-12 align-self-end">
+                                            <p class="m-0 p-2">
+                                                Episodes:
+                                                @foreach ($character['episode'] as $episode)
+                                                    <span>{{ basename($episode) }}</span>
+                                                    @if (!$loop->last)<span> | </span> @endif
+                                                @endforeach
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    @else
+                        <div class="col">
+                            <p>No results</p>
+                        </div>
+                    @endisset
+                </div>
+            </div>
+        </main>
 
     </body>
 </html>
